5b7aea774f989f2e73940192ee01b0df9d28955e
mtesting <mtesting66@gmail.com>
2017-12-28 15:22:40 +0000

    updated the project

--- MyDataBaseProject/src/mydata/Myconnection.java
+++ MyDataBaseProject/src/mydata/Myconnection.java
@@ -1,23 +1,51 @@
 package mydata;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.DriverManager;
 
 import org.junit.Test;
 
-import com.mysql.jdbc.Connection;
+
 
 public class Myconnection {
 	@Test
 		public void myConnection()
 		{
 		Connection conn=null;
-		String url="jdbc:mysql://localhost:3306/";
+		String url="jdbc:mysql://localhost:3306/userlogin";
 		String database="userlogin";
 		String username="root";
-		String password="root";
+		String password="hello123";
+		Statement s=null;
 		
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
+			 conn = DriverManager.getConnection(url, username, password);
+			 String createTable = "CREATE TABLE Login (" 
+				        + "Username VARCHAR(10),"  
+				        + "Password VARCHAR(8))" ;
+			 s=conn.createStatement();
+			 //s.executeUpdate(createTable);
+			 s.executeUpdate("insert into Login(Username,Password)values('Girish','girish')");
+			ResultSet rs= s.executeQuery("select * from Login");
+			while(rs.next())
+			{
+				String str = rs.getString("Username");
+				String str1=rs.getString("Password");
+				
+				
+			
+			System.out.println(str+"  "+str1);
+			}
+	
+			 
+				         
 		}
 		catch(Exception e){
 			System.out.println(e);
